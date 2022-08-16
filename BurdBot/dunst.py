b2163b9f774e2d4d884843ced3419128513d132c
Luke Hatcher <70284246+lukehatcherRP@users.noreply.github.com>
2022-08-16 15:41:39 -0400

    Create dunst.py

---
+++ BurdBot/dunst.py
@@ -0,0 +1,18 @@
+
+sentance = input('Enter sentance:\n')
+
+def dunst (sent):  
+    res = ""    
+    for idx in range(len(sent)):
+        if not idx % 2 :
+            res = res + sent[idx].upper()
+        else:
+            res = res + sent[idx].lower()
+    return(res)
+
+# printing result
+print("The alternate case string is : " + dunst(sentance))
+
+
+
+input()
